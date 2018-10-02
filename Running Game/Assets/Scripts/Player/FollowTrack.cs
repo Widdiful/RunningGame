a93976acb8ead5e00e3e024ebf7213f8156ef30f
Widdiful <Widdiful@users.noreply.github.com>
2018-10-02 17:04:08 +0100

    End of lesson

--- Running Game/Assets/Scripts/Player/FollowTrack.cs
+++ Running Game/Assets/Scripts/Player/FollowTrack.cs
@@ -11,8 +11,15 @@ public class FollowTrack : MonoBehaviour {
     public float speedIncreaseCountdown;
     public float speedIncreaseRate;
     public float horizontalAdjust;
+    public float moveSpeed;
 
     private int currentCurve = 0;
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        MoveTarget(1);
+    }
 
     private void Update() {
         if (speedIncreaseCountdown <= 0) {
@@ -21,10 +28,15 @@ public class FollowTrack : MonoBehaviour {
         else {
             speedIncreaseCountdown -= Time.deltaTime;
         }
-        MoveForward(speed);
+        MoveTowardsTarget();
+
+        if (true) { 
+            MoveTarget(speed);
+        }
+
     }
 
-    private void MoveForward(float amount) {
+    private void MoveTarget(float amount) {
         progress += (Time.deltaTime / spline.GetLengthOfCurve(currentCurve / 3)) * amount;
         if (progress >= 1f) {
             if (currentCurve / 3 < spline.CurveCount - 1) {
@@ -42,11 +54,27 @@ public class FollowTrack : MonoBehaviour {
                 }
             }
         }
+        targetPosition = spline.GetPointOnCurve(currentCurve, progress);
+
+        /*
         Vector3 position = spline.GetPointOnCurve(currentCurve, progress);
         transform.localPosition = position;
 
         if (lookForward) {
             transform.LookAt(position + spline.GetVelocityOnCurve(currentCurve, progress).normalized);
+        }
+        transform.Translate(new Vector3(horizontalAdjust, 0, 0), Space.Self);
+        */
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed);
+        transform.localPosition = position;
+
+        if (lookForward)
+        {
+            transform.LookAt(targetPosition);
         }
         transform.Translate(new Vector3(horizontalAdjust, 0, 0), Space.Self);
     }
