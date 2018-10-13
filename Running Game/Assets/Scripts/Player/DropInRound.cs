02e76b2c87864a23f9bb2743b3867767bacce5e1
radlog <33016059+radlog@users.noreply.github.com>
2018-10-13 23:37:58 +0100

    WallPlacement
    created simple script for wall placement and updated prefabs

--- Running Game/Assets/Scripts/Player/DropInRound.cs
+++ Running Game/Assets/Scripts/Player/DropInRound.cs
@@ -65,12 +65,6 @@ public class DropInRound : MonoBehaviour
 
         }
 
-        
-        
-   
-
-
-
         Destroy(GetComponent<DropInRound>());
     }
 
---
+++ Running Game/Assets/Scripts/Walls/WallPlacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPlacement : MonoBehaviour {
+
+    private List<GameObject> walls;
+    private SplineCurve spline;
+    private int maxWalls;
+
+	// Use this for initialization
+	void Start () {
+        walls = new List<GameObject>();
+        spline = GameObject.FindGameObjectWithTag("Path").GetComponent<SplineCurve>();
+        
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
