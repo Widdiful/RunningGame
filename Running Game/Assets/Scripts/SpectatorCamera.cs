48afcc2cb6630f8d1333ad075052f95e48d56463
Widdiful <Widdiful@users.noreply.github.com>
2018-10-19 17:02:25 +0100

    Pushing

---
+++ Running Game/Assets/Scripts/SpectatorCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorCamera : MonoBehaviour {
+    public float changeTime;
+    public float lerpSpeed;
+    private float changeTimer;
+    private Transform target;
+
+    private void Update()
+    {
+        changeTimer -= Time.fixedDeltaTime;
+        if (changeTimer <= 0)
+        {
+            changeTimer = changeTime;
+            target = FindObjectsOfType<FollowTrack>()[Random.Range(0, FindObjectsOfType<FollowTrack>().Length)].transform;
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), lerpSpeed);
+    }
+}
