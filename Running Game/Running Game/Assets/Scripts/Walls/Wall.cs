2ae6fc85316fa451338444e6723c28dd6efe04fa
FrankJones223 <1jonef50@solent.ac.uk>
2018-10-23 16:01:10 +0100

    Sound Update
    Change Log:
    Empty game object added to main scene housing Audio Source Element and new script holding code to play sound on game play for background music.
    Wall prefab ammended, 2 Audio sources added for Pass / !Pass.
    Wall script ammended, Check for Pass / !Pass updated to play relevent sound clip on check validation.

---
+++ Running Game/Running Game/Assets/Scripts/Walls/Wall.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    public AudioClip Swooshing;
+    public AudioClip Clang;
+    public SplineCurve attachedSpline;
+    [Range(0, 1)]
+    public float positionOnSpline;
+    private bool broken;
+
+    void Start()
+    {
+        positionOnSpline = attachedSpline.GetPositionOnSpline(transform.position);
+        transform.position = attachedSpline.GetPoint(positionOnSpline);
+        transform.LookAt(transform.position + attachedSpline.GetDirection(positionOnSpline));
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("broken");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!broken)
+            {
+                BreakWall(other.gameObject.GetComponent<Posing>().GetPose());
+                other.gameObject.GetComponent<PlayerStats>().Passed();
+                AudioSource.PlayClipAtPoint(Swooshing, transform.position, 0.6f);
+                //Debug.Log("broken");
+            }
+            else
+            {
+                if (gameObject.GetComponent<MeshFilter>().sharedMesh.name.Contains(other.gameObject.GetComponent<Posing>().GetPose()))
+                {
+                    other.gameObject.GetComponent<PlayerStats>().Passed();
+                    AudioSource.PlayClipAtPoint(Swooshing, transform.position, 0.6f);
+                }
+                else
+                {
+                    other.gameObject.GetComponent<PlayerStats>().Failed();
+                    AudioSource.PlayClipAtPoint(Clang, transform.position, 0.7f);
+                }
+            }
+        }
+
+    }
+
+    private void BreakWall(string pose)
+    {
+       // Debug.Log("Wall_" + pose);
+        gameObject.GetComponent<MeshFilter>().sharedMesh =
+            Resources.Load<GameObject>("Walls\\Wall_" + pose).GetComponent<MeshFilter>().sharedMesh;
+        broken = true;
+    }
+}
