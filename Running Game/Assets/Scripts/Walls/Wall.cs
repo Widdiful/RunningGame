3567dab3e5e0d7b46ddb197e67f460ac49a5d830
radlog <33016059+radlog@users.noreply.github.com>
2018-10-23 17:04:39 +0100

    Revert "Merge branch 'master' of https://github.com/Widdiful/RunningGame"
    This reverts commit 5d79e9674f0148ab83f5f79d6afc23c0d7fb4c85, reversing
    changes made to 276d7d8910362a181544c4865c5fa8d96708513f.

---
+++ Running Game/Assets/Scripts/Walls/Wall.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    public SplineCurve attachedSpline;
+    [Range(0, 1)]
+    public float positionOnSpline;
+    private bool broken;
+    private float repairTime = 30.0f;
+    public float timer;
+    //private System.Random rnd;
+
+    void Start()
+    {
+        //rnd = new System.Random();
+        positionOnSpline = attachedSpline.GetPositionOnSpline(transform.position);
+        transform.position = attachedSpline.GetPoint(positionOnSpline);
+        transform.LookAt(transform.position + attachedSpline.GetDirection(positionOnSpline));
+        //rndTime = rnd.Next();
+
+    }
+
+    void Update()
+    {
+        if(timer > repairTime)
+        {
+            RepairWall();
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("broken");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!broken)
+            {
+                BreakWall(other.gameObject.GetComponent<Posing>().GetPose());
+                other.gameObject.GetComponent<PlayerStats>().Passed();
+                //Debug.Log("broken");
+            }
+            else
+            {
+                if (gameObject.GetComponent<MeshFilter>().sharedMesh.name.Contains(other.gameObject.GetComponent<Posing>().GetPose()))
+                {
+                    other.gameObject.GetComponent<PlayerStats>().Passed();
+                }
+                else
+                {
+                    other.gameObject.GetComponent<PlayerStats>().Failed();
+                }
+            }
+        }
+
+    }
+
+    private void BreakWall(string pose)
+    {
+       // Debug.Log("Wall_" + pose);
+        gameObject.GetComponent<MeshFilter>().sharedMesh =
+            Resources.Load<GameObject>("Walls\\Wall_" + pose).GetComponent<MeshFilter>().sharedMesh;
+        broken = true;
+    }
+
+    private void RepairWall()
+    {
+        gameObject.GetComponent<MeshFilter>().sharedMesh =
+    Resources.Load<GameObject>("Walls\\Wall").GetComponent<MeshFilter>().sharedMesh;
+        broken = false;
+    }
+}
