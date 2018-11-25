54e467b42c1bfa262e7bdfc68dd9b03770a7752d
radlog <33016059+radlog@users.noreply.github.com>
2018-11-25 21:10:48 +0000

    added background music

--- Running Game/Assets/Scripts/GameData.cs
+++ Running Game/Assets/Scripts/GameData.cs
@@ -7,11 +7,13 @@ public class GameData : MonoBehaviour {
     public List<GameObject> players;
     public Camera cam;
     private Menu menu;
+    public AudioSource music;
 	// Use this for initialization
 	void Start () {
         players = new List<GameObject>();
         cam = GameObject.Find("MainCamera").GetComponent<Camera>();
         menu = GameObject.Find("MainMenu").GetComponent<Menu>();
+        music = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
